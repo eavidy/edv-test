a0b7e0e8f50f76064e5acf847373e539f373ac64
Eavid <ganrn@vip.qq.com>
2023-12-30 16:14:37 +0800

    up

--- .github/scripts/publish.ts
+++ .github/scripts/publish.ts
@@ -1,5 +1,9 @@
-import { publishFlow } from "https://esm.sh/gh/dnpack/action-script/cmd/github_repo.ts?raw";
+import {
+  GitHubRepo,
+  publishFlow,
+} from "https://esm.sh/gh/dnpack/action-script/cmd/github_repo.ts?raw";
 
-import packageJson from "../../package.json" with { type: "json" };
 console.log("::endgroup::");
-await publishFlow("v" + packageJson.version, { tagFailThrow: true });
+await publishFlow(["v0.3.1", "v0.3.2"], { tagFailThrow: true });
+const repo = new GitHubRepo();
+await repo.deleteMatchVersionTag(new Set(["v0.2.9"]), "0.2.10", "patch");
