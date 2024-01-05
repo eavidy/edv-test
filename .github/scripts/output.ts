f6773feaadbd46d31528e80bf8d6355690d92eb1
ganrinong <ganrinong@datagrand.com>
2024-01-05 09:57:39 +0800

    update

--- .github/scripts/output.ts
+++ .github/scripts/output.ts
@@ -1,3 +1,4 @@
-
 import { writeNewTagToOutput } from "https://cdn.jsdelivr.net/gh/dnpack/action-script@0.3.0/cmd/github_repo.ts";
 await writeNewTagToOutput(Math.random().toString(), "NEW_TAG");
+import { setOutput } from "npm:@actions/core";
+setOutput("YY", "111");
